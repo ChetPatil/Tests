30fb993c7e3d22abfa194f2570381ef0e0b9d9c5
Chetan Patil <97183122+ChetPatil@users.noreply.github.com>
2022-03-06 19:54:12 +0530

    Add files via upload

---
+++ Test_06-03/index.js
@@ -0,0 +1,42 @@
+function Details(id) {
+    appdetails(id);
+  }
+  
+  function appdetails(id) {
+    if (id == "1") {
+    let oppo = document.getElementById("1");
+    oppo.innerHTML =
+      "<h2>oppo reno</h2><br><p>₹26,003 - ₹29,999</p><br><p>fast-charging productivity</p><br><p>The best battery, upto 48 hours of game play</p><br><p>12cm(5-inch) amoled display</p>";
+  }
+   
+    if (id == "2") {
+        let vivo = document.getElementById("2");
+        vivo.innerHTML =
+        "<h2>vivo yt20</h2><br><p>₹12,003 - ₹15,503</p><br><p>13.7 cm (5-inch) Retina XDR display1</p><br><p>Best Camera shifts focus automatically in your videos</p><br><p>Advanced dual-camera system with 102MP Wide and Ultra Wide cameras; Photographic designs, Smart HDR</p>";
+    }
+    if (id == "3") {
+      let oneplus = document.getElementById("3");
+      oneplus.innerHTML =
+      "<h2>OnePlus 5</h2><br><p>₹10,999 - ₹15,999</p><br><p>Camera with shallow depth of field and shifts focus automatically in videos</p><br><p>Advanced dual-camera system with Wide cameras; Smart HDR</p><br>";
+    }
+
+    if (id == "4") {
+        let samsung1 = document.getElementById("4");
+        samsung1.innerHTML =
+        "<h2>Samsung Galaxy M21</h2><br><p>₹22,003 - ₹55,503</p><br><p>16.21cm (6.4-inch) Super Retina XDR display1</p><br><p>Cinematic mode adds shallow depth of field and shifts focus automatically in your videos</p><br><p>Water resistant to a depth of 6 meters for up to 30 minutes</p>";
+    }
+
+    if (id == "5") {
+        let iPhone = document.getElementById("5");
+        iPhone.innerHTML =
+        "<h2>Apple iPhone13 pro</h2><br><p>₹126,003 - ₹174,503</p><br><p>7MP TrueDepth Camera.</p><br><p>Talk time (wireless): Up to 25 hours; Internet use: Up to 15 hours; Video playback (wireless): Up to 16 hours</p><br><p>All-glass and aluminum design, water and dust resistant (rated IP67)</p>";
+    }
+
+    if (id == "6") {
+        let samsung2 = document.getElementById("6");
+        samsung2.innerHTML =
+        "<h2>Samsung Gallexy F12</h2><br><p>₹26,999 - ₹74,503</p><br><p>7MP TrueDepth Camera.</p><br><p>Talk time (wireless): Up to 25 hours; Internet use: Up to 15 hours; Video playback (wireless): Up to 16 hours</p><br><p>Water resistant to a depth of 6 meters for up to 30 minutes</p>";
+    }
+
+    
+  }
